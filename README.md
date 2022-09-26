9ab3d069fdff03ee3a72a417bec5cc63d6721d2f
Les Aker <me@lesaker.org>
2022-09-26 12:48:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+puppet-teslamate
+===========
+
+[![Puppet Forge](https://img.shields.io/puppetforge/v/halyard/teslamate.svg)](https://forge.puppetlabs.com/halyard/teslamate)
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/halyard/puppet-teslamate/Build)](https://github.com/halyard/puppet-teslamate/actions)
+[![MIT Licensed](http://img.shields.io/badge/license-MIT-green.svg?style=flat)](https://tldrlegal.com/license/mit-license)
+
+Module to configure [teslamate](https://github.com/adriankumpf/teslamate). Does not create a new grafana instance; point your existing Grafana at the Postgres database.
+
+## Usage
+
+```puppet
+include teslamate
+```
+## License
+
+teslamate is released under the MIT License. See the bundled LICENSE file for details.
+
---
+++ manifests/init.pp
@@ -0,0 +1,78 @@
+# @summary Configure TeslaMate instance
+#
+# @param datadir handles storage of Postgres data
+# @param mqttdir handles storage of mqtt messages
+# @param database_password sets the postgres password for teslamate
+# @param encryption_key sets the key used to encrypt tesla API tokens
+# @param teslamate_ip sets the IP of the teslamate container
+# @param postgres_ip sets the IP of the postgres container
+# @param mqtt_ip sets the IP of the mqtt container
+class teslamate (
+  String $datadir,
+  String $mqttdir,
+  String $database_password,
+  String $encryption_key,
+  String $teslamate_ip = '172.17.0.2',
+  String $postgres_ip = '172.17.0.3',
+  String $mqtt_ip = '172.17.0.4',
+) {
+  firewall { '100 dnat for postgres':
+    chain  => 'DOCKER_EXPOSE',
+    jump   => 'DNAT',
+    proto  => 'tcp',
+    dport  => 5432,
+    todest => "${postgres_ip}:5432",
+    table  => 'nat',
+  }
+
+  firewall { '100 dnat for mqtt':
+    chain  => 'DOCKER_EXPOSE',
+    jump   => 'DNAT',
+    proto  => 'tcp',
+    dport  => 1883,
+    todest => "${mqtt_ip}:1883",
+    table  => 'nat',
+  }
+
+  file { [$datadir, $mqttdir, "${mqttdir}/data", "${mqttdir}/config"]:
+    ensure => directory,
+  }
+
+  docker::container { 'postgres':
+    image   => 'postgres:14',
+    args    => [
+      "--ip ${postgres_ip}",
+      "-v ${datadir}/data:/var/lib/postgresql/data",
+      '-e POSTGRES_USER=teslamate',
+      "-e POSTGRES_PASSWORD=${database_password}",
+      '-e POSTGRES_DB=teslamate',
+    ],
+    cmd     => '',
+    require => File["${datadir}/data"],
+  }
+
+  docker::container { 'mqtt':
+    image   => 'eclipse-mosquitto:2',
+    args    => [
+      "--ip ${mqtt_ip}",
+      "-v ${mqttdir}/data:/mosquitto/data",
+      "-v ${mqttdir}/config:/mosquitto/config",
+    ],
+    cmd     => 'mosquitto -c /mosquitto-no-auth.conf',
+    require => [File["${mqttdir}/data"], File["${mqttdir}/config"]],
+  }
+
+  docker::container { 'teslamate':
+    image => 'teslamate/teslamate:latest',
+    args  => [
+      "--ip ${teslamate_ip}",
+      "-e ENCRYPTION_KEY=${encryption_key}",
+      '-e DATABASE_USER=teslamate',
+      "-e DATABASE_PASS=${database_password}",
+      '-e DATABASE_NAME=teslamate',
+      "-e DATABASE_HOST=${postgres_ip}",
+      "-e MQTT_HOST=${mqtt_ip}",
+    ],
+    cmd   => '',
+  }
+}
